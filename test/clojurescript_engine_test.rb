afc78ec74fc1aa6f2458e00b5bbccf026c363fbc
Mike Dalessio <mike@csa.net>
2012-09-04 08:11:19 -0400

    Erecting test coverage around boolean clojurescript engine options.

--- test/clojurescript_engine_test.rb
+++ test/clojurescript_engine_test.rb
@@ -23,24 +23,32 @@ class ClojureScriptEngineTest < Test::Unit::TestCase
   end
 
   def test_convert_options
-    options = {:optimizations => :advanced,  :output_dir => "#{Dir.pwd}"}
+    options = {:optimizations => :advanced,  :output_dir => "#{Dir.pwd}", :pretty_print => true}
     engine = Clementine::ClojureScriptEngine.new("", "")
     opts = engine.convert_options(options)
+
+    assert_equal 3, opts.length
+    opts.keys.each {|k| assert_equal Java::clojure.lang.Keyword, k.class }
+    assert_equal [":optimizations", ":output-dir", ":pretty-print"], opts.keys.map(&:to_s).sort
+
     opts.each do |k, v|
-      assert_equal Java::clojure.lang.Keyword, k.class
-      assert k.to_s == ":optimizations" || k.to_s == ":output-dir"
-      assert v.to_s == ":advanced" || v.to_s == "#{Dir.pwd}"
+      case k.to_s
+      when ":optimizations" then assert_equal ":advanced", v.to_s
+      when ":output-dir"    then assert_equal "#{Dir.pwd}", v.to_s
+      when ":pretty-print"  then assert_equal "true", v.to_s
+      end
     end
   end
 
   def test_created_clojure_map
-    options = {:optimizations => :advanced,  :output_dir => "#{Dir.pwd}"}
+    options = {:optimizations => :advanced,  :output_dir => "#{Dir.pwd}", :pretty_print => true}
     engine = Clementine::ClojureScriptEngine.new("", "")
     opts = engine.convert_options(options)
     map = PersistentHashMap.create(opts)
+    assert_equal 3, map.length
     map.each do |k, v|
       assert_equal Java::clojure.lang.Keyword, k.class
-      assert k.to_s == ":optimizations" || k.to_s == ":output-dir"
+      assert [":optimizations", ":output-dir", ":pretty-print"].include? k.to_s
     end
   end
 end
